de3007435ad92ecc64e36430173b63eb5eb7c5b5
gd2016 <371049740@qq.com>
2020-09-23 15:53:53 +0800

    add host

--- src/plugins/topic.js
+++ src/plugins/topic.js
@@ -90,7 +90,7 @@ export default class Topic {
         size: '10',
         ...this.params
       },
-      domainName: '',
+      domainName: this.host,
       emitFocusEvent: () => {
         this.topicId = ''
         this.name = ''
